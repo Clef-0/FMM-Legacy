004ebbdbba667653249310229142550958578341
Clef-0 <spaceyavin@gmail.com>
2016-05-04 23:13:53 +0100

    Changing Window title to show that it's pre-release

--- FoundationMM/Window.Designer.cs
+++ FoundationMM/Window.Designer.cs
@@ -250,7 +250,7 @@
             this.Controls.Add(this.statusStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Window";
-            this.Text = "Foundation Mod Manager 1.00";
+            this.Text = "Foundation Mod Manager (pre-release build)";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Window_FormClosing);
             this.Load += new System.EventHandler(this.Window_Load);
             this.panel1.ResumeLayout(false);
