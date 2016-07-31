7011142b1548bddf4b997e7186fab41c1f7d53b6
Clef-0 <spaceyavin@gmail.com>
2016-07-31 19:59:55 +0100

    unstole someone's code and used a faster alternative

--- FoundationMM/worker_FileTransfer.cs
+++ FoundationMM/worker_FileTransfer.cs
@@ -63,56 +63,44 @@ namespace FoundationMM
             }
         }
         
-        // borrowed from James Johnson @ StackOverflow because Clef's code sucked ass
         private bool areBakAndMainEqual(string file1, string file2)
         {
-            int file1byte;
-            int file2byte;
-            FileStream fs1;
-            FileStream fs2;
-
-            // Determine if the same file was referenced two times.
             if (file1 == file2)
             {
-                // Return true to indicate that the files are the same.
                 return true;
             }
 
-            // Open the two files.
-            fs1 = new FileStream(file1, FileMode.Open, FileAccess.Read);
-            fs2 = new FileStream(file2, FileMode.Open, FileAccess.Read);
-
-            // Check the file sizes. If they are not the same, the files 
-            // are not the same.
-            if (fs1.Length != fs2.Length)
+            using (FileStream fs1 = File.OpenRead(file1))
             {
-                // Close the file
-                fs1.Close();
-                fs2.Close();
+                using (FileStream fs2 = File.OpenRead(file2))
+                {
+                    if (fs1.Length != fs2.Length)
+                    {
+                        return false;
+                    }
 
-                // Return false to indicate files are different
-                return false;
+                    int count;
+                    const int size = 0x1000000;
+
+                    var buffer = new byte[size];
+                    var buffer2 = new byte[size];
+
+                    while ((count = fs1.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fs2.Read(buffer2, 0, buffer2.Length);
+
+                        for (int i = 0; i < count; i++)
+                        {
+                            if (buffer[i] != buffer2[i])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
             }
 
-            // Read and compare a byte from each file until either a
-            // non-matching set of bytes is found or until the end of
-            // file1 is reached.
-            do
-            {
-                // Read one byte from each file.
-                file1byte = fs1.ReadByte();
-                file2byte = fs2.ReadByte();
-            }
-            while ((file1byte == file2byte) && (file1byte != -1));
-
-            // Close the files.
-            fs1.Close();
-            fs2.Close();
-
-            // Return the success of the comparison. "file1byte" is 
-            // equal to "file2byte" at this point only if the files are 
-            // the same.
-            return ((file1byte - file2byte) == 0);
+            return true;
         }
 
         private void fileTransferWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
