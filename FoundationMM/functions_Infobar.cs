66c520b56a0c4bb5f1da09aefc09dfcc585adfb3
Clef-0 <spaceyavin@gmail.com>
2016-08-05 13:10:23 +0100

    full offline mode

--- FoundationMM/functions_Infobar.cs
+++ FoundationMM/functions_Infobar.cs
@@ -87,26 +87,36 @@ namespace FoundationMM
                 }
                 else
                 {
-                    try
+                    if (ini.IniReadValue("FMMPrefs", "OfflineMode").ToLower() != "true")
                     {
-                        infobar.ColumnStyles[0].Width = 72;
-                        System.Net.WebRequest request = System.Net.WebRequest.Create(ini.IniReadValue("FMMInfo", "Icon"));
-                        System.Net.WebResponse response = request.GetResponse();
-                        System.IO.Stream responseStream = response.GetResponseStream();
-                        infobarIcon.BackgroundImage = new Bitmap(responseStream);
-                        infobarIcon.BackgroundImageLayout = ImageLayout.Center;
+                        try
+                        {
+                            infobar.ColumnStyles[0].Width = 72;
+                            System.Net.WebRequest request = System.Net.WebRequest.Create(ini.IniReadValue("FMMInfo", "Icon"));
+                            System.Net.WebResponse response = request.GetResponse();
+                            System.IO.Stream responseStream = response.GetResponseStream();
+                            infobarIcon.BackgroundImage = new Bitmap(responseStream);
+                            infobarIcon.BackgroundImageLayout = ImageLayout.Center;
 
-                        infobarIcon.Cursor = Cursors.Hand;
-                        if (ini.IniReadValue("FMMInfo", "Url") != "")
-                        {
-                            infobarIcon.Tag = ini.IniReadValue("FMMInfo", "Url");
+                            infobarIcon.Cursor = Cursors.Hand;
+                            if (ini.IniReadValue("FMMInfo", "Url") != "")
+                            {
+                                infobarIcon.Tag = ini.IniReadValue("FMMInfo", "Url");
+                            }
+                            else
+                            {
+                                infobarIcon.Tag = ini.IniReadValue("FMMInfo", "Icon");
+                            }
                         }
-                        else
+                        catch
                         {
-                            infobarIcon.Tag = ini.IniReadValue("FMMInfo", "Icon");
+                            infobar.ColumnStyles[0].Width = 0;
+                            infobarIcon.BackgroundImage = null;
+                            infobarIcon.Cursor = null;
+                            infobarIcon.Tag = "";
                         }
                     }
-                    catch
+                    else
                     {
                         infobar.ColumnStyles[0].Width = 0;
                         infobarIcon.BackgroundImage = null;
@@ -124,26 +134,36 @@ namespace FoundationMM
                 }
                 else
                 {
-                    try
+                    if (ini.IniReadValue("FMMPrefs", "OfflineMode").ToLower() != "true")
                     {
-                        infobar.ColumnStyles[3].Width = 128;
-                        System.Net.WebRequest request = System.Net.WebRequest.Create(ini.IniReadValue("FMMInfo", "ImageThumb"));
-                        System.Net.WebResponse response = request.GetResponse();
-                        System.IO.Stream responseStream = response.GetResponseStream();
-                        infobarImage.BackgroundImage = new Bitmap(responseStream);
-                        infobarImage.BackgroundImageLayout = ImageLayout.Center;
+                        try
+                        {
+                            infobar.ColumnStyles[3].Width = 128;
+                            System.Net.WebRequest request = System.Net.WebRequest.Create(ini.IniReadValue("FMMInfo", "ImageThumb"));
+                            System.Net.WebResponse response = request.GetResponse();
+                            System.IO.Stream responseStream = response.GetResponseStream();
+                            infobarImage.BackgroundImage = new Bitmap(responseStream);
+                            infobarImage.BackgroundImageLayout = ImageLayout.Center;
 
-                        infobarImage.Cursor = Cursors.Hand;
-                        if (ini.IniReadValue("FMMInfo", "ImageFull") != "")
-                        {
-                            infobarImage.Tag = ini.IniReadValue("FMMInfo", "ImageFull");
+                            infobarImage.Cursor = Cursors.Hand;
+                            if (ini.IniReadValue("FMMInfo", "ImageFull") != "")
+                            {
+                                infobarImage.Tag = ini.IniReadValue("FMMInfo", "ImageFull");
+                            }
+                            else
+                            {
+                                infobarImage.Tag = ini.IniReadValue("FMMInfo", "ImageThumb");
+                            }
                         }
-                        else
+                        catch
                         {
-                            infobarImage.Tag = ini.IniReadValue("FMMInfo", "ImageThumb");
+                            infobar.ColumnStyles[3].Width = 0;
+                            infobarImage.BackgroundImage = null;
+                            infobarImage.Cursor = null;
+                            infobarImage.Tag = "";
                         }
                     }
-                    catch
+                    else
                     {
                         infobar.ColumnStyles[3].Width = 0;
                         infobarImage.BackgroundImage = null;
@@ -222,26 +242,36 @@ namespace FoundationMM
                 }
                 else
                 {
-                    try
+                    if (ini.IniReadValue("FMMPrefs", "OfflineMode").ToLower() != "true")
                     {
-                        infobar2.ColumnStyles[0].Width = 72;
-                        System.Net.WebRequest request = System.Net.WebRequest.Create(ini.IniReadValue("FMMInfo", "Icon"));
-                        System.Net.WebResponse response = request.GetResponse();
-                        System.IO.Stream responseStream = response.GetResponseStream();
-                        infobar2Icon.BackgroundImage = new Bitmap(responseStream);
-                        infobar2Icon.BackgroundImageLayout = ImageLayout.Center;
+                        try
+                        {
+                            infobar2.ColumnStyles[0].Width = 72;
+                            System.Net.WebRequest request = System.Net.WebRequest.Create(ini.IniReadValue("FMMInfo", "Icon"));
+                            System.Net.WebResponse response = request.GetResponse();
+                            System.IO.Stream responseStream = response.GetResponseStream();
+                            infobar2Icon.BackgroundImage = new Bitmap(responseStream);
+                            infobar2Icon.BackgroundImageLayout = ImageLayout.Center;
 
-                        infobar2Icon.Cursor = Cursors.Hand;
-                        if (ini.IniReadValue("FMMInfo", "Url") != "")
-                        {
-                            infobar2Icon.Tag = ini.IniReadValue("FMMInfo", "Url");
+                            infobar2Icon.Cursor = Cursors.Hand;
+                            if (ini.IniReadValue("FMMInfo", "Url") != "")
+                            {
+                                infobar2Icon.Tag = ini.IniReadValue("FMMInfo", "Url");
+                            }
+                            else
+                            {
+                                infobar2Icon.Tag = ini.IniReadValue("FMMInfo", "Icon");
+                            }
                         }
-                        else
+                        catch
                         {
-                            infobar2Icon.Tag = ini.IniReadValue("FMMInfo", "Icon");
+                            infobar2.ColumnStyles[0].Width = 0;
+                            infobar2Icon.BackgroundImage = null;
+                            infobar2Icon.Cursor = null;
+                            infobar2Icon.Tag = "";
                         }
                     }
-                    catch
+                    else
                     {
                         infobar2.ColumnStyles[0].Width = 0;
                         infobar2Icon.BackgroundImage = null;
@@ -259,26 +289,36 @@ namespace FoundationMM
                 }
                 else
                 {
-                    try
+                    if (ini.IniReadValue("FMMPrefs", "OfflineMode").ToLower() != "true")
                     {
-                        infobar2.ColumnStyles[3].Width = 128;
-                        System.Net.WebRequest request = System.Net.WebRequest.Create(ini.IniReadValue("FMMInfo", "ImageThumb"));
-                        System.Net.WebResponse response = request.GetResponse();
-                        System.IO.Stream responseStream = response.GetResponseStream();
-                        infobar2Image.BackgroundImage = new Bitmap(responseStream);
-                        infobar2Image.BackgroundImageLayout = ImageLayout.Center;
+                        try
+                        {
+                            infobar2.ColumnStyles[3].Width = 128;
+                            System.Net.WebRequest request = System.Net.WebRequest.Create(ini.IniReadValue("FMMInfo", "ImageThumb"));
+                            System.Net.WebResponse response = request.GetResponse();
+                            System.IO.Stream responseStream = response.GetResponseStream();
+                            infobar2Image.BackgroundImage = new Bitmap(responseStream);
+                            infobar2Image.BackgroundImageLayout = ImageLayout.Center;
 
-                        infobar2Image.Cursor = Cursors.Hand;
-                        if (ini.IniReadValue("FMMInfo", "ImageFull") != "")
-                        {
-                            infobar2Image.Tag = ini.IniReadValue("FMMInfo", "ImageFull");
+                            infobar2Image.Cursor = Cursors.Hand;
+                            if (ini.IniReadValue("FMMInfo", "ImageFull") != "")
+                            {
+                                infobar2Image.Tag = ini.IniReadValue("FMMInfo", "ImageFull");
+                            }
+                            else
+                            {
+                                infobar2Image.Tag = ini.IniReadValue("FMMInfo", "ImageThumb");
+                            }
                         }
-                        else
+                        catch
                         {
-                            infobar2Image.Tag = ini.IniReadValue("FMMInfo", "ImageThumb");
+                            infobar2.ColumnStyles[3].Width = 0;
+                            infobar2Image.BackgroundImage = null;
+                            infobar2Image.Cursor = null;
+                            infobar2Image.Tag = "";
                         }
                     }
-                    catch
+                    else
                     {
                         infobar2.ColumnStyles[3].Width = 0;
                         infobar2Image.BackgroundImage = null;
